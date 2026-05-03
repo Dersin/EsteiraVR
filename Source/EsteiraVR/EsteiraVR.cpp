4a06396574f3c7389b5c2c010471fa4fd5c0871c
Dersin <anderson_cardoso2001@hotmail.com>
2026-05-03 17:56:21 -0300

    fist commit

---
+++ Source/EsteiraVR/EsteiraVR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EsteiraVR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EsteiraVR, "EsteiraVR" );
